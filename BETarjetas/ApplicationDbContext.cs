b322e31e10b54e145fb49a7c5aa51ec6ec0c2277
Johnkel <Johnkel@gmail.com>
2021-07-12 13:00:07 -0400

    Mi Primera API WEB

---
+++ BETarjetas/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BETarjetas.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BETarjetas
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<TarjetaCredito> TarjetaCreditos { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ BETarjetas/Migrations/20210705165156_v1.0.0.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BETarjetas.Migrations
+{
+    public partial class v100 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TarjetaCreditos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titular = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroTarjeta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaExpiracion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cvv = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TarjetaCreditos", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TarjetaCreditos");
+        }
+    }
+}
